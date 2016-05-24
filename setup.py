af62990a27f1250f82b92ab3fb4a848df0dac880
Gökhan Sarı <me@th0th.me>
2016-05-24 14:17:33 +0300

    bump version to 0.0.5

--- setup.py
+++ setup.py
@@ -7,13 +7,13 @@ with open(os.path.join(os.path.dirname(__file__), "README.rst")) as readme:
 
 setup(
     name='metu-cafeteria-menu',
-    version='0.0.4',
+    version='0.0.5',
     packages=['metu_cafeteria_menu'],
     description="A utility for fetching Middle East Technical University's cafeteria menu.",
     test_suite='tests',
     keywords=['metu', 'cafeteria', 'menu'],
     url='https://github.com/th0th/metu-cafeteria-menu',
-    download_url='https://github.com/th0th/metu-cafeteria-menu/tarball/0.0.4',
+    download_url='https://github.com/th0th/metu-cafeteria-menu/tarball/0.0.5',
     author=u'Gökhan Sarı',
     author_email='me@th0th.me',
     license='MIT',
